42544b8d7f7e6490ad4ae40db26cf1c7e56b0cc3
Paulo Quinete <paulo.quinete@gmail.com>
2025-01-26 17:59:33 -0300

    .

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and htps in. Allow everrything out."
 
-  vpc_id = data.aws_vpc_default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
