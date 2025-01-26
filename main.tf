902f20114ef229fce73d821b5d84441e68e9f0ec
pauloquinete <43891466+pauloquinete@users.noreply.github.com>
2025-01-26 19:36:16 -0300

    Auto scalling

--- main.tf
+++ main.tf
@@ -33,17 +33,22 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.0.1"
+ 
+  name = "blog"
+  min_size = 1
+  max_size = 2
+  
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_group_ids  = [module.blog_sg.security_group_id]
 
-   subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Blog Test - HelloWorld"
-  }
+  image_id            = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
+
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
