a4ea4aacd5b1021d5045a89ce1e1ee692e9a62af
pauloquinete <43891466+pauloquinete@users.noreply.github.com>
2025-01-27 20:47:45 -0300

    copy and paste

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = var.ami_filter.name
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,8 +11,9 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = var.ami_filter.owner
+  owners = [var.ami_filter.owner] # Bitnami
 }
+
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -37,8 +38,8 @@ module "blog_autoscaling" {
 
   name = "blog"
 
-  min_size            = var.asg_min_size
-  max_size            = var.asg_max_size
+  min_size            = var.asg_min
+  max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
@@ -60,7 +61,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "${var.environment.name}-"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -76,7 +77,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = var.environment.name
+    Environment = "dev"
   }
 }
 
@@ -85,7 +86,7 @@ module "blog_sg" {
   version = "4.13.0"
 
   vpc_id  = module.blog_vpc.vpc_id
-  name    = "${var.environment.name}-blog"
+  name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
